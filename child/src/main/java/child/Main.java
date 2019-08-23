a1e00d2e18119d6799297f2146f1ce005d9a9f0d
hung.m.nguyen <hung.m.nguyen@opswat.com>
2019-08-23 11:49:50 +0700

    add child - parent Project

---
+++ child/src/main/java/child/Main.java
@@ -0,0 +1,13 @@
+package child;
+
+public class Main {
+
+	public static void sayHello() {
+		System.out.println("Hello world");
+	}
+	
+	public static void sayBye() {
+		System.out.println("Bye...");
+	}
+	
+}
---
+++ parent/src/main/java/parent/MainParrent.java
@@ -0,0 +1,12 @@
+package parent;
+
+import child.Main;
+
+public class MainParrent {
+
+	public static void main(String[] args) {
+		Main.sayHello();
+		Main.sayBye();
+	}
+	
+}
